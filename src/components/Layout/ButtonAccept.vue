b5c8464dce246bd2de25a27face98a032cc7feb9
jeikerb <jeiker.balaguera@codingtel.com>
2022-04-11 20:49:23 -0400

    botones

---
+++ src/components/Layout/ButtonAccept.vue
@@ -0,0 +1,19 @@
+<template>
+    <button 
+        class="border bg-green-500 border-green-600 hover:bg-green-600 rounded-md p-2 mx-2"
+    >
+        {{ content }}
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: 'ButtonAcept',
+
+    props:[
+        'content'
+    ]
+
+}
+</script>
---
+++ src/components/Layout/ButtonCancel.vue
@@ -0,0 +1,19 @@
+<template>
+    <button 
+        class="border bg-red-500 border-red-600 hover:bg-red-600 rounded-md p-2 mx-2"
+    >
+        {{ content }}
+    </button>
+</template>
+
+<script>
+
+export default {
+    name: 'ButtonCancel',
+
+    props: [
+        'content'
+    ],
+
+}
+</script>
